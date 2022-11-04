f76ac4c8b8d3a7d172a424a72e75465276bbe4a9
bytss <jeffersonalaiza@yahoo.com>
2022-11-05 00:47:21 +0800

    v2ray

--- install.sh
+++ install.sh
@@ -802,7 +802,7 @@ install_v2ray() {
 		cp -rf $(pwd)/* /etc/v2ray/233boy/v2ray
 	else
 		pushd /tmp
-		git clone https://github.com/bytss/v2ray -b "main" /etc/v2ray/233boy/v2ray --depth=1
+		git clone https://github.com/bytss/v2ray-install -b "$_gitbranch" /etc/v2ray/233boy/v2ray --depth=1
 		popd
 
 	fi
@@ -990,7 +990,7 @@ _gitbranch=$2
 case $args in
 online)
 	#hello world
-	[[ -z $_gitbranch ]] && _gitbranch="master"
+	[[ -z $_gitbranch ]] && _gitbranch="main"
 	;;
 local)
 	local_install=true
