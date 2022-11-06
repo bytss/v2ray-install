f3c6fb04fde42da3b43f2030fda052f106d53f33
bytss <jeffersonalaiza@yahoo.com>
2022-11-06 14:27:10 +0800

    uuid

--- v2ray.sh
+++ v2ray.sh
@@ -212,11 +212,11 @@ add_uuid_client() {
 	while getopts u: flag
 	do
 	 case "$flag" in
-	 	u) uuid=${OPTARG};;
+	 	u) guuid=${OPTARG};;
 	 esac
 	done
 
-	sed -i '12 a \                                        {\n                                                "id": "'${uuid}'",\n                                                "level": 1,\n                                                "alterId": 0\n                                        },' $v2ray_server_config
+	sed -i '12 a \                                        {\n                                                "id": "'${guuid}'",\n                                                "level": 1,\n                                                "alterId": 0\n                                        },' $v2ray_server_config
 	echo "Added!"
 	restart_v2ray
 }
