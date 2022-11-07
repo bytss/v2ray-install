5598145d2f9105e009fdda534fda0f7e9ea212eb
bytss <jeffersonalaiza@yahoo.com>
2022-11-07 12:56:43 +0800

    uuid

--- v2ray.sh
+++ v2ray.sh
@@ -221,7 +221,7 @@ done
 
 sed -i '12 a \                                        {\n                                                "id": "'${guuid}'",\n                                                "level": 1,\n                                                "alterId": 0\n                                        },' $v2ray_server_config
 echo "Added!"
-systemctl restart v2ray
+restart_v2ray
 
 chmod +x /usr/bin/clientadd
 }
