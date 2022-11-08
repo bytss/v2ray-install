4fb34594ad12f90844679d32314138d8f98a10ba
bytss <jeffersonalaiza@yahoo.com>
2022-11-08 13:26:44 +0800

    uuid delete

--- v2ray.sh
+++ v2ray.sh
@@ -104,6 +104,7 @@ if [[ ! $(grep 'run -config' /lib/systemd/system/v2ray.service)  && $v2ray_ver_v
 	systemctl daemon-reload
 	systemctl restart v2ray
 	v2ray_client_add
+	v2ray_client_delete
 fi
 
 # fix caddy2 config
